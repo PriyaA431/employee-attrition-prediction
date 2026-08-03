2caa7d0f1236bd59314b9a27cd36a1cacbabbac6
Priya H P <priyagowda0403@gmail.com>
2026-08-04 01:25:52 +0530

    visualization

--- src/visualize.py
+++ src/visualize.py
@@ -22,7 +22,7 @@ plt.title("Employee Attrition Distribution")
 # Show plot
 plt.show()
 
-
+#======================================
 
 plt.figure(figsize=(8, 5))
 
@@ -31,5 +31,49 @@ sns.histplot(df["Age"], bins=20, kde=True)
 plt.title("Age Distribution of Employees")
 plt.xlabel("Age")
 plt.ylabel("Number of Employees")
+
+plt.show()
+
+#--------------------------------------
+
+plt.figure(figsize=(6,4))
+
+sns.countplot(data=df, x="Gender")
+
+plt.title("Gender Distribution")
+
+plt.show()
+
+
+#====================================
+
+plt.figure(figsize=(7,4))
+sns.countplot(data = df, x="Department")
+plt.title("Department Distribution")
+plt.xticks(rotation=20)
+plt.show()
+
+
+#+++++++++++++++++++++++++++++++
+
+plt.figure(figsize=(12,5))
+
+sns.countplot(data=df, x="JobRole")
+
+plt.xticks(rotation=45)
+
+plt.title("Job Role Distribution")
+
+plt.show()
+
+
+
+#+++++++++++++============
+
+plt.figure(figsize=(8,5))
+
+sns.histplot(df["MonthlyIncome"], bins=30, kde=True)
+
+plt.title("Monthly Income Distribution")
 
 plt.show()
